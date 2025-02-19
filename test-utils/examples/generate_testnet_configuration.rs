600413488c176ea70a1ad004ca02323c99e692b0
peer2 <peer2@templarprotocol.com>
2025-02-19 16:29:29 +0900

    chore: remove master account from testnet config

--- test-utils/examples/generate_testnet_configuration.rs
+++ test-utils/examples/generate_testnet_configuration.rs
@@ -1,21 +1,16 @@
 //! Used by GitHub Actions to generate default market configuration.
 
-use near_sdk::{serde_json, AccountId};
+use near_sdk::serde_json;
 use templar_common::market::YieldWeights;
 use test_utils::market_configuration;
 
 pub fn main() {
-    let master_account: AccountId = std::env::var("NEAR_CONTRACT_STAGING_ACCOUNT_ID")
-        .unwrap()
-        .parse()
-        .unwrap();
-
     println!(
         "{{\"configuration\":{}}}",
         serde_json::to_string(&market_configuration(
             "usdt.fakes.testnet".parse().unwrap(),
             "wrap.testnet".parse().unwrap(),
-            YieldWeights::new_with_supply_weight(9).with_static(master_account, 1)
+            YieldWeights::new_with_supply_weight(1),
         ))
         .unwrap(),
     );
